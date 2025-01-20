dbe4b6bc94d372d5e4dafa0214c62908f1f37641
Adil Ahmed <adil@Mac.communityfibre.co.uk>
2025-01-20 22:05:00 +0000

    Adding the initial query selectors i need. Creating the first function for the random position of the food.

--- snake-game/src/main.ts
+++ snake-game/src/main.ts
@@ -1,5 +1,20 @@
 import './style.css'
 
-const gameBoard = document.querySelector(".game-board"); // document method querySelector() return the first element within the document that matches the specified CSS selector.
-const scoreElement = document.querySelector(".score");
-const highScoreElement = document.querySelector(".highScore");
+const gameBoard = document.querySelector<HTMLInputElement>("#display"); // document method querySelector() return the first element within the document that matches the specified CSS selector.
+const scoreElement = document.querySelector<>(".score");
+const highScoreElement = document.querySelector<>(".highScore");
+const controlsElement = document.querySelectorAll<HTMLButtonElement>(); // Will i need this because there are multiple button.
+
+let foodX, foodY; // Creating a variable for food. It should have a positing on the grid. x for row, y for column.
+// What other variables do i need???
+// Position of snake when the game starts
+// Score
+// Length of the snake body
+// Game Over
+
+//Function for the food position
+const newFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 10 * 30); // gives a random x position on the grid of 30.
+    foodY = Math.floor(Math.random() * 10 * 30);
+}
+
